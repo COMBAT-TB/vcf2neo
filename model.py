fe9fecb00835fc41f543b58c03ab3c730c5d4047
thoba <lose.thoba@gmail.com>
2016-01-21 12:28:29 +0200

    Added more Nodes.

--- model.py
+++ model.py
@@ -13,7 +13,7 @@ class Gene(StructuredNode):
     locus_tag = StringProperty(index=True)
     gene_synonym = StringProperty(index=True)
     coding = BooleanProperty()  # Coding (Sub-Feature=CDS) or Non-Coding (Sub-Feature != CDS)
-    protein_id = StringProperty()
+    protein_id = StringProperty(index=True)
     start = IntegerProperty()
     end = IntegerProperty()
     strand = StringProperty()
@@ -39,9 +39,35 @@ class Transcript(StructuredNode):
     _type = StringProperty(index=True)  # I am planning to group these types [ncRNA, tRNA, rRNA, transcript]
     product = StringProperty(index=True)
     parent = StringProperty()  # The Parent gene
-    gene = StringProperty()
+    gene = StringProperty(index=True)
     note = StringProperty(index=True)
     sequence = StringProperty()
+
+
+class CDS(StructuredNode):
+    """
+    CDS
+    """
+    print 'CDS Nodes'
+    cds_id = StringProperty(Unique_Index=True)
+    name = StringProperty(Unique_Index=True, index=True)
+    gene_id = StringProperty(Unique_Index=True)
+    product = StringProperty(index=True)
+    protein_id = StringProperty(index=True)
+
+
+class Exon(StructuredNode):
+    """
+    Exon
+    """
+    print 'Exon Nodes'
+    exon_id = StringProperty()
+    location = StringProperty()
+    _type = StringProperty()
+    gene_id = StringProperty(index=True)
+    parent = StringProperty()
+    product = StringProperty(index=True)
+    note = StringProperty()
 
 
 class Protein(StructuredNode):
