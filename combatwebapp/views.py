f4233b07e0f9ee1005e4bd805237a2ad471aa068
thoba <lose.thoba@gmail.com>
2016-03-14 14:53:40 +0200

    Embeding JBrowse in result page.

--- combatwebapp/views.py
+++ combatwebapp/views.py
@@ -12,22 +12,43 @@ def search_nodes(name):
         node = Gene.nodes.get(name=name)
         return node
     except DoesNotExist, e:
-        raise e
+        pass
+    try:
+        print 'Searching Pseudogene Nodes with Gene_ID=', name
+        node = Pseudogene.nodes.get(gene_id=name)
+        return node
+    except DoesNotExist, e:
+        pass
+    try:
+        print 'Searching Transcript Nodes with Gene=', name
+        node = Transcript.nodes.get(gene=name)
+        return node
+    except DoesNotExist, e:
+        pass
+    try:
+        print 'Searching Protein Node with Parent=', name
+        node = Protein.nodes.get(parent=name)
+        return node
+    except DoesNotExist, e:
+        pass
+    return 'Nothing found!'
 
 
 @app.route('/')
 def index():
-    # gene = search_nodes('Rv0016c')
     return render_template('index.html')
 
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    gene = None
+    gene = name = location = None
     if request.method == 'POST':
         term = request.form['gene']
         gene = search_nodes(term)
-    return render_template('results.html', gene=gene.name)
+        location = str(gene.start)+'..'+str(gene.end)
+        for ortholog in gene.has_ortholog.match():
+            name = ortholog.locus_name
+    return render_template('results.html', gene=gene.name, name=name, location=location)
 
 
 @app.route('/about')
