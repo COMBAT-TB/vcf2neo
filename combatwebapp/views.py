b832eaf08a34eaec35de1e5d8013aaca3e520460
thoba <lose.thoba@gmail.com>
2016-03-18 18:02:19 +0200

    Integrated more information.

--- combatwebapp/views.py
+++ combatwebapp/views.py
@@ -1,21 +1,35 @@
 from combat_tb_model import *
-from neomodel import DoesNotExist
+from neomodel import DoesNotExist, db
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+
+def cypher_search(name):
+    print 'cypher_search...'
+    res = None
+    query = "MATCH (n:Gene) where n.name=~'(?i){}.*' OR n.locus_tag=~'(?i){}.*' " \
+            "OR n.preffered_name=~'(?i){}.*' OR n.uniprot_entry=~'(?i){}.*' RETURN n".format(name, name, name, name)
+    result, meta = db.cypher_query(query)
+    for row in result:
+        res = Gene.inflate(row[0])
+    return res
 
 
 # Search nodes
 def search_nodes(name):
     try:
         print 'Searching Gene Nodes with Name=', name
-        node = Gene.nodes.get(name=name)
+        # node = Gene.nodes.get(name=name)
+        node = cypher_search(name)
+        if node:
+            print node.name
         return node
     except DoesNotExist, e:
         pass
     try:
         print 'Searching Pseudogene Nodes with Gene_ID=', name
-        node = Pseudogene.nodes.get(gene_id=name)
+        node = Pseudogene.nodes.get(pseudogene_id='gene:' + name)
         return node
     except DoesNotExist, e:
         pass
@@ -41,10 +55,12 @@ def index():
 
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    term = gene = pseudogene = ortholog_name = location = None
+    term = gene = pseudogene = ortholog_name = location = protein = None
+    go_terms = []
+    inter_pro = []
     if request.method == 'POST':
         term = request.form['gene']
-        gene = search_nodes(term.capitalize())
+        gene = search_nodes(term)
         class_name = gene.__class__.__name__
         print class_name
         if gene:
@@ -52,10 +68,17 @@ def search():
             if 'Ps' not in class_name:
                 for ortholog in gene.has_ortholog.match():
                     ortholog_name = ortholog.locus_name
+                for go in gene.has_go_terms.match():
+                    go_terms.append(str(go.go_id))
+                for inter in gene.has_interpro_terms.match():
+                    inter_pro.append(str(inter.interpro_id))
+                for cdc in gene.translated.match():
+                    for prot in cdc.translated_.match():
+                        protein = prot
             elif 'Ps' in class_name:
                 pseudogene = gene.biotype
     return render_template('results.html', term=term, gene=gene, pseudogene=pseudogene, ortholog_name=ortholog_name,
-                           location=location)
+                           location=location, go_terms=go_terms, inter_pro=inter_pro, protein=protein)
 
 
 @app.route('/about')
