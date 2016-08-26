2690f411075d710dd21b24208b1e24eed53ada25
Thoba Lose <lose.thoba@gmail.com>
2016-08-26 10:10:58 +0200

    Minor refactoring

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from os import getenv
 
 from BCBio import GFF
-from py2neo import Graph, watch, Node
+from py2neo import Graph, Node
+from tqdm import tqdm
+
+from model.model import Feature
 
 # https://neo4j.com/developer/kb/explanation-of-error-on-session-connection-using-uniform-drivers/
 graph = Graph(password=getenv("NEO4J_PASSWORD", ""), encrypted=False)
-watch("neo4j.bolt")
+
+
+# watch("neo4j.bolt")
 
 
 def delete_data():
@@ -13,8 +18,8 @@ def delete_data():
     Delete existing data
     :return:
     """
-    print "Deleting all nodes and relationships..."
-    return graph.run("MATCH (n) DETACH DELETE n").dump()
+    print("Deleting all nodes and relationships...")
+    graph.run("MATCH (n) DETACH DELETE n").dump()
 
 
 def get_feature_name(feature):
@@ -41,9 +46,9 @@ def create_feature_nodes(feature):
     names = get_feature_name(feature)
     name = names.get("Name", names.get("UniqueName"))
     unique_name = names.get("UniqueName", name)
-    _feature = Node("Feature", name=name, type=feature.type, uniquename=unique_name)
+    _feature = Node(Feature.__name__, name=name, type=feature.type, uniquename=unique_name)
     graph.create(_feature)
-    print("CREATED:", _feature)
+    # print("CREATED:", _feature)
 
 
 def load_gff_data(gff_file, limit):
@@ -54,16 +59,10 @@ def load_gff_data(gff_file, limit):
     :return:
     """
     in_file = open(gff_file)
-    print("About to parse gff_file and load to graph...")
-    print("LIMIT:", limit)
     limit_info = dict(gff_type=limit)
-
     for rec in GFF.parse(gff_file, limit_info=limit_info):
-        for feature in rec.features:
-            if feature.type == 'gene':
-                create_feature_nodes(feature)
-            else:
-                create_feature_nodes(feature)
+        for feature in tqdm(rec.features):
+            create_feature_nodes(feature)
     in_file.close()
 
 
@@ -75,12 +74,12 @@ def parse_gff():
     gff_file = "MTB_H37rv.gff3"
     limits = [["gene", "pseudogene", "exon", "tRNA_gene", "ncRNA_gene", "rRNA_gene"], ["transcript"], ["CDS"]]
     for limit in limits:
-        print("Loading", limit, " to database...")
+        print("Loading", limit, "...")
         load_gff_data(gff_file, limit)
-        print("Done loading", limit, "features to database")
+    print("Done.")
 
 
 if __name__ == '__main__':
     delete_data()
     parse_gff()
-    print(graph)
+    # print(graph)
