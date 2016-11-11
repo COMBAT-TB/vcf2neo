64e5dd84ae2f2c4bf9bc0d39d548890614da2dcb
Thoba Lose <lose.thoba@gmail.com>
2016-11-11 12:24:23 +0200

    Refactoring codebase for py2neo

--- combatwebapp/views.py
+++ combatwebapp/views.py
@@ -4,15 +4,16 @@ import json
 import os
 import sys
 
+from bioblend import galaxy
+from bioblend.galaxy.client import ConnectionError
+from bioblend.galaxy.datasets import DatasetClient, DatasetTimeoutException
 from flask import Flask, Response, render_template, request
-from neomodel import DoesNotExist, db
+from py2neo import Graph, getenv
 
-from neomodel.cardinality import CardinalityViolation
 from combat_tb_model.model import *
 from gsea import enrichment_analysis
-from bioblend import galaxy
-from bioblend.galaxy.datasets import DatasetClient, DatasetTimeoutException
-from bioblend.galaxy.client import ConnectionError
+
+graph = Graph(host=getenv("DB", "localhost"), bolt=True, password=getenv("NEO4J_PASSWORD", ""))
 
 app = Flask(__name__)
 
@@ -34,77 +35,6 @@ def connect_to_galaxy(galaxy_url='http://demo.sanbi.ac.za/', api_key=None):
         api_key = os.environ.get('GALAXY_API_KEY')
     gi = galaxy.GalaxyInstance(url=galaxy_url, key=api_key)
     return gi
-
-
-def cypher_search(name):
-    print('cypher_search...')
-    arr = []
-    res = None
-    query = "MATCH (n:Gene) where n.name=~'(?i){}.*' OR n.locus_tag=~'(?i){}.*' " \
-            "OR n.preffered_name=~'(?i){}.*' OR n.uniprot_entry=~'(?i){}.*' RETURN n " \
-            "UNION MATCH (n:Pseudogene) where n.name=~'(?i){}.*' RETURN n " \
-            "UNION MATCH (n:Gene)-[:ORTHOLOG]->(o:Ortholog) where o.locus_name=~'(?i){}.*' RETURN n " \
-            "".format(name, name, name, name, name, name)
-    result, meta = db.cypher_query(query)
-    for row in result:
-        res = Gene.inflate(row[0])
-        arr.append(res)
-    print(len(arr))
-    print([entry.name for entry in arr])
-    return arr
-
-
-# Search nodes
-def search_nodes(name):
-    try:
-        print('Searching Gene Nodes with Name=', name)
-        # node = Gene.nodes.get(name=name)
-        node = cypher_search(name)
-        if node:
-            print(node)
-        return node
-    except DoesNotExist as e:
-        pass
-    try:
-        print('Searching Pseudogene Nodes with Gene_ID=', name)
-        node = Pseudogene.nodes.get(pseudogene_id='gene:' + name)
-        return node
-    except DoesNotExist as e:
-        pass
-    try:
-        print('Searching Transcript Nodes with Gene=', name)
-        node = Transcript.nodes.get(gene=name)
-        return node
-    except DoesNotExist as e:
-        pass
-    try:
-        print('Searching Protein Node with Parent=', name)
-        node = Protein.nodes.get(parent=name)
-        return node
-    except DoesNotExist as e:
-        pass
-    return None
-
-
-def search_node(name):
-    nodes = []
-    try:
-        print('Searching Gene Nodes with Name=', name)
-        node = Gene.nodes.get(name=name)
-        if node:
-            print(node)
-            nodes.append(node)
-        return nodes
-    except DoesNotExist as e:
-        try:
-            print('Searching Pseudogene Nodes with Name=', name)
-            node = Pseudogene.nodes.get(name=name)
-            if node:
-                print(node)
-                nodes.append(node)
-            return nodes
-        except DoesNotExist as e:
-            raise e
 
 
 def find_interacting_proteins(locus_tag):
@@ -176,7 +106,7 @@ def find_interacting_proteins(locus_tag):
         for partner_protein in tb_interactions:
             query = 'MATCH (g:Gene) -[:TRANSCRIBED]-> () -[:PROCESSED_INTO]-> () -[:TRANSLATED]-> ' \
                     '(:Protein {{uniprot_id: "{}"}}) RETURN g.name'.format(partner_protein.uniprot_id)
-            (result, _) = db.cypher_query(query)
+            (result, _) = graph.data(query)
             gene_name = result[0][0]
             print("gene_name: ", gene_name, file=sys.stderr)
             edges.append(dict(data=dict(id='{}_{}'.format(protein.protein_id, partner_protein.protein_id),
@@ -186,11 +116,11 @@ def find_interacting_proteins(locus_tag):
             interactions_graph.extend(go_term_subgraph(partner_protein, go_terms_seen))
             interactions_graph.extend(interpro_term_subgraph(partner_protein, interpro_terms_seen))
         interactions_graph.extend(edges)
-    except CardinalityViolation:
+    except Exception:
         tb_interactions = []
     try:
         human_interactions = protein.interacts_.all()
-    except CardinalityViolation:
+    except Exception:
         human_interactions = []
     # print('gene:', gene, protein, tb_interactions, human_interactions, file=sys.stderr)
     interactions_graph_str = json.dumps(interactions_graph)
@@ -212,14 +142,46 @@ def find_interacting_proteins(locus_tag):
     return dict(elements=interactions_graph, styles=styles)
 
 
+def search_feature(name):
+    """
+    Search Feature Nodes
+    :param name:
+    :return:
+    """
+    try:
+        print('Searching Feature Nodes with Name=', name)
+        feature = list(
+            Feature.select(graph).where("_.name =~'(?i){}.*' OR _.uniquename=~'gene:(?i){}.*'".format(name, name)))
+    except Exception as e:
+        raise e
+    finally:
+        return feature
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
+def search_gene(uniquename):
+    """
+    Search Gene Nodes
+    :param uniquename:
+    :return:
+    """
+    try:
+        print('Searching Gene Nodes with Name=', uniquename)
+        gene = Gene.select(graph).where(
+            "_.name =~'(?i){}.*' OR _.uniquename=~'(?i){}.*'".format(uniquename, uniquename))
+    except Exception as e:
+        raise e
+    finally:
+        return gene
+
+
 @app.route('/search', methods=['GET', 'POST'])
 def search():
-    gene = term = pseudogene = ortholog_name = protein = interact = h_interact = None
+    gene = term = PseudoGene = ortholog_name = protein = interact = h_interact = None
     go_terms = []
     inter_pro = []
     ints = []
@@ -230,64 +192,66 @@ def search():
     print('ITEMS:', request.args.items())
     if request.method == 'GET':
         term = request.args.get('gene')
-        gene = search_node(term)
+        gene = search_gene(term)
     if request.method == 'POST':
         term = request.form['gene']
-        gene = search_nodes(term)
-    print(term)
-    class_name = gene.__class__.__name__
-    print(class_name)
-    if gene and len(gene) > 1:
-        print(len(gene))
-        length = len(gene)
-        print('Gene is an array...', len(gene))
-        return render_template('m_results.html', genes=gene, length=length)
-    elif gene and len(gene) == 1:
+        features = search_feature(term)
+        for feature in features:
+            if 'gene:' in feature.uniquename:
+                gene = search_gene(feature.uniquename)
+                location = feature.location
+                # goterms = feature.cvterms
+                # for go in goterms:
+                #     print(go.name)
+                for loc in location:
+                    print(loc.fmin, loc.fmax)
+                    location = str(str(loc.fmin) + '..' + str(loc.fmax))
+    # if gene and len(gene) > 1:
+    #     print(len(gene))
+    #     length = len(gene)
+    #     print('Gene is an array...', len(gene))
+    #     return render_template('m_results.html', genes=gene, length=length)
+    if gene:
         # Trying to zoom out
-        location = str(int(gene[0].start)) + '..' + str(int(gene[0].end))
-        if 'Ps' not in gene[0].__class__.__name__:
-            print(gene[0].__class__.__name__)
-            for ortholog in gene[0].has_ortholog.match():
-                ortholog_name = ortholog.locus_name
-            for go in gene[0].has_go_terms.match():
-                go_terms.append(go)
-            for inter in gene[0].has_interpro_terms.match():
-                inter_pro.append(str(inter.interpro_id))
-            for cdc in gene[0].translated.match():
-                for prot in cdc.translated_.match():
-                    protein = prot
-            try:
-                for actor in protein.interacts.match():
-                    ints.append(actor)
-            except Exception as e:
-                pass
-            try:
-                for h_actor in protein.interacts_.match():
-                    h_ints.append(h_actor)
-            except Exception as e:
-                pass
-            interact = [a.uniprot_id for a in ints]
-            h_interact = [a.protein_id for a in h_ints]
-            if gene[0].citation:
-                # Dealing with unicode
-                citation = gene[0].citation.encode('utf-8').replace('[', '').replace(']', '').split(', ')
-                cite = [ct[1:-1] for ct in citation]
-                for entry in cite:
-                    if len(entry) > 0:
-                        pub = Publication.nodes.get(pubmed_id=entry)
-                        publications.append(pub)
-                # Dealing with Unicode
-                for p in publications:
-                    au = p.authors.encode('utf-8').replace('[', '').replace(']', '').split(', ')
-                    aus[p.pubmed_id] = [a[1:-1] for a in au]
-                # Dealing with Unicode
-                structure_ids = protein.pdb_id.encode('utf-8').replace('[', '').replace(']', '').split(', ')
-                pdb_ids = [struc[2:-1] for struc in structure_ids]
+        # location = str(int(gene[0].location[0].fmin)) + '..' + str(int(gene[0].location[0].fmax))
+        # for ortholog in gene[0].has_ortholog.match():
+        #     ortholog_name = ortholog.locus_name
+        # for go in feature.cvterm:
+        #     go_terms.append(go)
+        # for inter in gene[0].has_interpro_terms.match():
+        #     inter_pro.append(str(inter.interpro_id))
+        # for cdc in gene[0].translated.match():
+        #     for prot in cdc.translated_.match():
+        #         protein = prot
+        # try:
+        #     for actor in protein.interacts.match():
+        #         ints.append(actor)
+        # except Exception as e:
+        #     pass
+        # try:
+        #     for h_actor in protein.interacts_.match():
+        #         h_ints.append(h_actor)
+        # except Exception as e:
+        #     pass
+        # interact = [a.uniprot_id for a in ints]
+        # h_interact = [a.protein_id for a in h_ints]
+        # if gene[0].citation:
+        #     # Dealing with unicode
+        #     citation = gene[0].citation.encode('utf-8').replace('[', '').replace(']', '').split(', ')
+        #     cite = [ct[1:-1] for ct in citation]
+        #     for entry in cite:
+        #         if len(entry) > 0:
+        #             pub = Publication.nodes.get(pubmed_id=entry)
+        #             publications.append(pub)
+        #     # Dealing with Unicode
+        #     for p in publications:
+        #         au = p.authors.encode('utf-8').replace('[', '').replace(']', '').split(', ')
+        #         aus[p.pubmed_id] = [a[1:-1] for a in au]
+        #     # Dealing with Unicode
+        #     structure_ids = protein.pdb_id.encode('utf-8').replace('[', '').replace(']', '').split(', ')
+        #     pdb_ids = [struc[2:-1] for struc in structure_ids]
 
-        elif 'Ps' in gene[0].__class__.__name__:
-            pseudogene = gene[0].biotype
-
-        return render_template('results.html', term=term, gene=gene[0], pseudogene=pseudogene,
+        return render_template('results.html', term=term, gene=gene, PseudoGene=PseudoGene,
                                ortholog_name=ortholog_name, citation=publications, authors=aus, pdb_ids=pdb_ids,
                                location=location, go_terms=go_terms, inter_pro=inter_pro, protein=protein,
                                interactor=interact, h_interact=h_interact)
@@ -375,8 +339,8 @@ def galaxy_histories():
         print(e, file=sys.stderr)
         hist_list = []
     else:
-        hist_list = [history for history in gi.histories.get_histories() if history.get('deleted') == False and
-                     history.get('purged') == False]
+        hist_list = [history for history in gi.histories.get_histories() if history.get('deleted') is False and
+                     history.get('purged') is False]
     return Response(
         json.dumps(hist_list),
         mimetype='application/json',
@@ -396,8 +360,8 @@ def galaxy_dataset(history_id):
         dataset_list = []
     else:
         dataset_list = [dataset for dataset in gi.histories.show_history(history_id, contents=True) if
-                        dataset.get('extension') == 'txt' and dataset.get('deleted') == False and dataset.get(
-                            'purged') == False]
+                        dataset.get('extension') == 'txt' and dataset.get('deleted') is False and dataset.get(
+                            'purged') is False]
 
     return Response(
         json.dumps(dataset_list),
