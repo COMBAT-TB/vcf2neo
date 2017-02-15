b0ccd6f383383b253b62f6800ab4305618722404
Thoba Lose <thobalose@users.noreply.github.com>
2017-02-15 14:00:49 +0200

    Update vcfproc.py

--- vcf2neo/vcfproc.py
+++ vcf2neo/vcfproc.py
@@ -17,12 +17,12 @@ class Vcf(object):
         self.vcf_dir = vcf_dir
 
     def process(self):
-        sys.stderr.write("We have the following VCF files in directory ({}):\n".format(self.vcf_dir))
+        sys.stdout.write("We have the following VCF files in directory ({}):\n".format(self.vcf_dir))
         print("We have the following VCF files in directory ({}):\n".format(self.vcf_dir))
         for vcf_file in glob.glob(self.vcf_dir + "/*.vcf"):
             print(vcf_file)
-            sys.stderr.write("Processing: {}!".format(vcf_file))
-            print("Processing: {}!".format(vcf_file))
+            sys.stderr.write("Processing: {}!\n".format(vcf_file))
+            print("Processing: {}!\n".format(vcf_file))
             start = time.time()
             vcf_reader = vcf.Reader(open(vcf_file, 'r'))
             # TODO: Have a standard way of identifying variant_set_names
@@ -32,9 +32,10 @@ class Vcf(object):
             create_call_set_nodes(set_name=vcf_file_name)
             self.get_variant_sites(vcf_reader, vcf_file_name)
             end = time.time()
-            sys.stderr.write("Processed {} in {}!".format(vcf_file_name.upper(), end - start))
-            print("Processed {} in {}!".format(vcf_file_name.upper(), end - start))
+            sys.stderr.write("Processed {} in {}!\n".format(vcf_file_name.upper(), end - start))
+            print("Processed {} in {}!\n".format(vcf_file_name.upper(), end - start))
             time.sleep(2)
+        sys.stderr.write("Done Processing!\n")
 
     def get_variant_sites(self, vcf_reader=None, vcf_file_name=None):
         for record in vcf_reader:
