fd948aa21ec6e18e68dcc375eb7b777d9a437b9d
Thoba Lose <thobalose@users.noreply.github.com>
2017-02-16 15:37:38 +0200

    Update vcfproc.py

--- vcf2neo/vcfproc.py
+++ vcf2neo/vcfproc.py
@@ -28,22 +28,24 @@ class Vcf(object):
             vcf_reader = vcf.Reader(open(vcf_file, 'r'))
             # TODO: Have a standard way of identifying variant_set_names
             vcf_file_name = str(vcf_file).replace(str(self.vcf_dir) + "/", "")
+            vset_name = str(self.vcf_dir).split('/')[-1]
             # TODO: Let's use the file name for now
-            create_variant_set_nodes(set_name=str(self.vcf_dir).split('/')[-1])
-            create_call_set_nodes(set_name=vcf_file_name)
-            self.get_variant_sites(vcf_reader=vcf_reader, vcf_file_name=vcf_file_name)
+            create_variant_set_nodes(set_name=vset_name)
+            create_call_set_nodes(set_name=vcf_file_name, vset=vset_name)
+            self.get_variant_sites(vcf_reader=vcf_reader, vcf_file_name=vset_name)
             end = time.time()
             sys.stderr.write("Processed {} in {}!\n".format(vcf_file_name.upper(), end - start))
             print("Processed {} in {}!\n".format(vcf_file_name.upper(), end - start))
             time.sleep(2)
         sys.stderr.write("Done Processing!\n")
+        print("Done Processing!\n")
 
-    def get_variant_sites(self, vcf_reader=None, vcf_file_name=None):
+    def get_variant_sites(self, vcf_reader=None, vset_name=None):
         for record in vcf_reader:
             print("\n")
             print(record)
             annotation = self.get_variant_ann(record=record)
-            create_variant_site_nodes(record, annotation, vcf_file_name)
+            create_variant_site_nodes(record, annotation, vset_name)
 
     @staticmethod
     def get_variant_ann(record=None):
