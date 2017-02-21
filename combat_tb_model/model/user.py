963caeef046704abfce0d333c42729bcbf6a243a
Thoba Lose <lose.thoba@gmail.com>
2017-02-21 09:31:54 +0200

    About to add updated model as subtree.

--- combat_tb_model/model/user.py
+++
@@ -1,8 +0,0 @@
-from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
-from vcf import VariantSet
-
-class User(GraphObject):
-    username = Property()
-    email = Property()
-
-    owns = RelatedTo("VariantSet", "OWNS_SET")
