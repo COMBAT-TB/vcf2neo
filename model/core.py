18bb27e6badbdfa2da3462f1792f7940dfaaa377
Thoba Lose <lose.thoba@gmail.com>
2017-02-24 13:18:20 +0200

    Merge branch 'fasta_import' into py2neo

--- model/core.py
+++ model/core.py
@@ -110,6 +110,7 @@ class Chromosome(Feature):
 
     is_a = RelatedTo("Feature", "IS_A")
 
+
 class Polypeptide(Feature):
     # more commonly known as a Protein - we should call it that - pvh
     so_id = "SO:0000104"
@@ -229,86 +230,3 @@ class DbXref(GraphObject):
         self.version = version
         self.db = db
         self.description = description
-
-class VariantSet(GraphObject):
-    __primarykey__ = 'name'
-    name = Property()
-
-    has_var = RelatedTo("VariantSite", "HAS_VAR")
-    has_call = RelatedTo("Call", "HAS_CALL")
-
-    def __init__(self, name):
-        self.name = name
-
-
-# VariantSite = Variant
-class VariantSite(GraphObject):
-    # __primarykey__ = 'pos'
-
-    pos = Property()
-    feature_id = Property()
-    biotype = Property()
-    chrom = Property()
-    ref_allele = Property()
-    alt_allele = Property()
-    quality = Property()
-    depth = Property()
-    gene = Property()
-    gene_id = Property()
-    known = Property()
-    novel = Property()
-
-    occurs_in = RelatedTo("Gene", "OCCURS_IN")
-    location = RelatedTo("FeatureLoc", "LOCATED_AT")
-
-    has_call = RelatedTo("Call", "HAS_CALL")
-    belongs_to_vset = RelatedTo("VariantSet", "BELONGS_TO_VSET")
-
-    def __init__(self, chrom, pos, ref_allele, alt_allele, gene=None):
-        self.chrom = chrom
-        self.pos = pos
-        self.ref_allele = ref_allele
-        self.alt_allele = alt_allele
-        self.gene = gene
-
-
-# CallSet = VCF file
-class CallSet(GraphObject):
-    __primarykey__ = 'name'
-    name = Property()
-
-    has_call = RelatedTo("Call", "HAS_CALL")
-    has_calls_in = RelatedTo("VariantSet", "HAS_CALLS_IN")
-
-    def __init__(self, name):
-        self.name = name
-
-
-class Call(GraphObject):
-    # __primarykey__ = 'pos'
-    genotype = Property()
-    ref_allele = Property()
-    alt_allele = Property()
-    gene = Property()
-    pos = Property()
-
-    associated_with = RelatedTo("VariantSite", "ASSOC_WITH_VARIANT")
-    belongs_to_cset = RelatedTo("CallSet", "BELONGS_TO_SET")
-
-    def __init__(self, pos, ref_allele, alt_allele, gene=None):
-        self.pos = pos
-        self.ref_allele = ref_allele
-        self.alt_allele = alt_allele
-        self.gene = gene
-
-
-class FastTree(GraphObject):
-    __primarykey__ = 'name'
-    name = Property()
-    data = Property()
-
-    has_var = RelatedTo("VariantSet", "HAS_VAR")
-
-    def __init__(self, name, data):
-        self.name = name
-        self.data = data
