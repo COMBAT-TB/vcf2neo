10e9ad3286fe7946bdc29413492a300d2f6ea214
Thoba Lose <lose.thoba@gmail.com>
2017-05-17 12:01:58 +0200

    Squashed 'vcf2neo/combat_tb_model/' changes from 21a7539..79531fb
    79531fb Incorporate FastTree class to the VCF model file
    3a2518a Merge from upstream
    e2f5ff1 * Fix naming of VCF relationships * Make imports relative
    f0a42e7 Merge commit '40f81ae827c59d972eebf9a83d3b2f2ede5865af' * Removed GalaxyJob from GalaxyUser
    af5a7e5 * switch impact to Call from VariantSite * load impact annotation to Call from VCF processing
    40f81ae Merge commit 'fbfb6856a242cf8a5247d8911cf6360fe0fd6f6a' into tree_view
    550d832 Added impact level to VSite.
    f68f100 Added __primarykey__.
    3f66b39 vcfproc and model updates.
    294765b Merge commit '6d630341d0528c1de8ddc58b0bd741419a595e2c' as 'vcf2neo/combat_tb_model'
    e048e70 Adding subtree
    36be760 imports.
    d2664a0 bump version.
    b368bc8 Merge branch 'celery_queque' of bitbucket.org:sanbidev/combattb_web into celery_queque
    bcf8e8b cahnges
    3bac6a8 Merge branch 'py2neo' into celery_queque
    8aaa416 removed build rel. call in cli.
    ce45897 removing uuids.
    cec38d8 push proc.
    4c482c3 Merged master.
    eacb23a merging master.
    803976f checkout v_cache.
    488fe3f Merge branch 'py2neo' into celery_queque
    a715133 Updates before deploy.
    4f21503 Merge commit '15af781e1d8ff525e746d60db21c09477cd35ed4'
    15af781 Squashed 'vcf2neo/combat_tb_model/' changes from 83b38cc..41686fd
    ad6e970 __primarykey__ notes.
    880e188 fixed conflicts.
    a59a5e8 Squashed 'vcf2neo/combat_tb_model/' changes from 270d0f2..83b38cc
    0655cd2 Imports and DB
    ba53363 will be using element identifier.
    95a87f0 Merge branch 'master' of github.com:SANBI-SA/vcf2neo
    cb1e889 Added network in compose.
    c98788a Update db.py
    d55e260 Import fixes.
    2f2d596 Squashed 'vcf2neo/combat_tb_model/' content from commit 270d0f2
    334cfbf Merge commit '2f2d596fc5c5724fab1b3f9b6affee331bd791cc' as 'vcf2neo/combat_tb_model'
    536e032 moving model.
    a38b2c4 Variant Cache idea
    18bc0d0 import
    6b89779 Imports
    8053333 Added owner as arg  in rel
    f66f5e4 Adding Init data to control changes in database.
    6367ceb .
    73c86d0 .
    58a6687 .
    ea2c151 history_id.
    aa03c5b conflict.
    d8120c1 Added history_id.
    ae8357f Fixed conflicts.
    bd64b0d fixing imports.
    0019a21 ...
    60865e7 update model imports.
    486eac5 Merge commit '404ee3556551d87492c4bc706d243ac0b9db06a0' as 'combat_tb_model'
    404ee35 Squashed 'combat_tb_model/' content from commit 460ccc0
    
    git-subtree-dir: vcf2neo/combat_tb_model
    git-subtree-split: 79531fb29fd16c79a425de3513a728c452777dd3

--- model/galaxyuser.py
+++ model/galaxyuser.py
@@ -1,5 +1,7 @@
 from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
-from vcfmodel import *
+#from vcfmodel import VariantSet
+
+from .vcfmodel import *
 
 
 class GalaxyUser(GraphObject):
@@ -10,21 +12,8 @@ class GalaxyUser(GraphObject):
     email = Property()
 
     owns = RelatedTo("VariantSet", "OWNS_SET")
-    has_job = RelatedTo("UserJob", "HAS_JOB")
 
     def __init__(self, user_key=None, email=None, username=None):
         self.user_key = user_key
         self.email = email
         self.username = username
-
-
-class UserJob(GraphObject):
-
-    user_key = Property()
-    task_id = Property()
-
-    belongs_to_user = RelatedTo("GalaxyUser", "BELONGS_TO_USER")
-
-    def __init__(self, user_key=None, task_id=None):
-        self.user_key = user_key
-        self.task_id = task_id
--- model/vcfmodel.py
+++ model/vcfmodel.py
@@ -1,8 +1,6 @@
-# from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
-from core import *
-from galaxyuser import GalaxyUser
-from fasttree import FastTree
-
+    # from py2neo.ogm import GraphObject, Property, RelatedTo, RelatedFrom
+from .core import *
+from .galaxyuser import GalaxyUser
 
 # class Phenotype(GraphObject):
 #     __primarykey__ = 'type'
@@ -19,10 +17,11 @@ class VariantSet(GraphObject):
     owner = Property()
     history_id = Property()
 
-    has_variant = RelatedTo("VariantSite", "HAS_VARIANT")
+    has_variant = RelatedFrom("VariantSite", "BELONGS_TO_VSET")
     has_call = RelatedTo("Call", "HAS_CALL")
     owned_by = RelatedFrom("GalaxyUser", "OWNS_SET")
     forms_tree = RelatedFrom("FastTree", "FROM_VARIANT_SET")
+    has_callsets = RelatedFrom("CallSet", "HAS_CALLS_IN")
 
     def __init__(self, name, owner, history_id=None):
         self.name = name
@@ -33,8 +32,8 @@ class VariantSet(GraphObject):
 # VariantSite = Variant
 class VariantSite(GraphObject):
     # NOTE: relies on FeatureLoc from core.py
-    # make __primarykey__ = VariantSite.name+POS
-    # __primarykey__ = 'pos'
+    # make __primarykey__ = VariantSet.name+POS
+    __primarykey__ = 'pk'
 
     pos = Property()
     feature_id = Property()
@@ -48,6 +47,7 @@ class VariantSite(GraphObject):
     gene_id = Property()
     known = Property()
     novel = Property()
+    pk = Property()
 
     occurs_in = RelatedTo("Gene", "OCCURS_IN")
     location = RelatedTo("FeatureLoc", "LOCATED_AT")
@@ -55,12 +55,13 @@ class VariantSite(GraphObject):
     has_call = RelatedTo("Call", "HAS_CALL")
     belongs_to_vset = RelatedTo("VariantSet", "BELONGS_TO_VSET")
 
-    def __init__(self, chrom, pos, ref_allele, alt_allele, gene=None):
+    def __init__(self, chrom, pos, ref_allele, alt_allele, pk, gene=None):
         self.chrom = chrom
         self.pos = pos
         self.ref_allele = ref_allele
         self.alt_allele = alt_allele
         self.gene = gene
+        self.pk = pk
 
 
 # CallSet = VCF file
@@ -70,28 +71,45 @@ class CallSet(GraphObject):
     vset = Property()
     identifier = Property()
 
-    has_call = RelatedTo("Call", "HAS_CALL")
+    has_call = RelatedFrom("Call", "BELONGS_TO_CSET")
     has_calls_in = RelatedTo("VariantSet", "HAS_CALLS_IN")
 
-    def __init__(self, name, vset):
+    def __init__(self, name):
         self.name = name
-        self.vset = vset
+        # self.vset = vset
 
 
 class Call(GraphObject):
-    # make __primarykey__ = CallSet.name+VariantSet.name
-    # __primarykey__ = 'pos'
+    # make __primarykey__ = CallSet.name+VariantSet.name+pos
+    __primarykey__ = 'pk'
     genotype = Property()
     ref_allele = Property()
     alt_allele = Property()
     gene = Property()
     pos = Property()
+    pk = Property()
+    impact = Property()
 
     associated_with = RelatedTo("VariantSite", "ASSOC_WITH_VARIANT")
-    belongs_to_cset = RelatedTo("CallSet", "BELONGS_TO_SET")
+    belongs_to_cset = RelatedTo("CallSet", "BELONGS_TO_CSET")
 
-    def __init__(self, pos, ref_allele, alt_allele, gene=None):
+    def __init__(self, pos, ref_allele, alt_allele, pk, impact, gene=None):
         self.pos = pos
         self.ref_allele = ref_allele
         self.alt_allele = alt_allele
+        self.impact = impact
         self.gene = gene
+        self.pk = pk
+
+class FastTree(GraphObject):
+    __primarykey__ = 'name'
+    name = Property()
+    data = Property()
+    history_id = Property()
+
+    from_variant_set = RelatedTo("VariantSet", "FROM_VARIANT_SET")
+
+    def __init__(self, name, data, history_id):
+        self.name = name
+        self.data = data
+        self.history_id = history_id
