3ead0362af82fcd1a5039441c99b7b2687fbe247
thoba <lose.thoba@gmail.com>
2019-05-28 16:09:06 +0200

    Fix for cset.

--- vcf2neo/db.py
+++ vcf2neo/db.py
@@ -96,9 +96,11 @@ class GraphDb(object):
                              pk=str(v_set.name) + str(pos),
                              impact=annotation[2])
             self.graph.create(v_site)
+            known_sites[pos] = v_site
+
+        if c_set:
             v_site.belongs_to_cset.add(c_set)
             c_set.has_variants.add(v_site)
-            known_sites[pos] = v_site
 
         gene = Gene.select(self.graph, str(v_site.gene)).first()
         if gene:
